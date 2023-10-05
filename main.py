12c5c83a815a73ef113b53c61f2e603efe254069
Lessar762 <97272645+Lessar762@users.noreply.github.com>
2023-10-05 19:49:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,8 +33,9 @@ class get_html(object):
         number=0
         if not os.path.exists("download_pic/"):
             os.mkdir("download_pic/")
-        for i in range(504,505):
-            os.mkdir("download_pic/"+self.heroname[number]+"/")
+        for i in List:
+            if not os.path.exists("download_pic/"+self.heroname[number]+"/"):
+                os.mkdir("download_pic/"+self.heroname[number]+"/")
             for num in range(15):#真的会有英雄有14个皮肤吗
                 num+=1
                 try:
@@ -43,8 +44,9 @@ class get_html(object):
                     with open("download_pic/"+self.heroname[number]+"/"+self.heroname[number]+str(num)+".jpg","wb") as f:
                         f.write(img)
                 except urllib.error.HTTPError:
+                    print(self.heroname[number]+"Successfully download!")
                     number+=1 
-                    break
+                    return
             
     
   
